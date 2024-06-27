ba098589a5e78417ad4fb4f3da92e56479475143
vishal kumar <vishalke035@gmail.com>
2024-06-27 14:56:08 +0530

    dynamic text is added

--- script.js
+++ script.js
@@ -1,4 +1,109 @@
-//search functionality
+//Code for loader(spinner)
+window.addEventListener("load", () => {
+  const loaderWrapper = document.getElementById("loader-wrapper");
+  const content = document.getElementById("content");
+
+  setTimeout(() => {
+    loaderWrapper.style.opacity = "0";
+    loaderWrapper.style.visibility = "hidden";
+    content.style.display = "block";
+    setTimeout(() => {
+      content.classList.add("loaded");
+      content.style.opacity = "1"; // Ensure content is fully visible
+    }, 100); // Slight delay to trigger the CSS transition
+  }, 3000);
+});
+
+//Code for dynamic text generation
+const dynamicText = document.getElementById("dynamicText");
+const cursor = document.getElementById("cursor");
+const textArray = [
+  "Tech Enthusiast",
+  "Efficient Coder",
+  "Full Stack Developer",
+  "Software Developer",
+  "Java Developer",
+];
+let index = 0;
+let letterIndex = 0;
+let text = "";
+let reverseMode = false;
+
+function updateText() {
+  if (!reverseMode) {
+    if (letterIndex < textArray[index].length) {
+      text += textArray[index].charAt(letterIndex);
+      dynamicText.textContent = text;
+      letterIndex++;
+    } else {
+      reverseMode = true;
+    }
+  } else {
+    if (letterIndex > 0) {
+      text = text.slice(0, -1);
+      dynamicText.textContent = text;
+      letterIndex--;
+    } else {
+      reverseMode = false;
+      index = (index + 1) % textArray.length;
+    }
+  }
+  const textWidth = dynamicText.offsetWidth;
+  const containerWidth = dynamicText.parentElement.offsetWidth;
+  const cursorOffset = (containerWidth - textWidth) / 2 + textWidth + 45;
+  cursor.style.left = cursorOffset + "px";
+}
+
+setInterval(updateText, 130);
+
+//Code for life-interest gallery section
+function showImages(category) {
+  const gallery = document.getElementById("gallery");
+  gallery.innerHTML = "";
+
+  const images =
+    category === "life"
+      ? [
+          "https://media.istockphoto.com/id/1202312179/photo/hands-cup-holding-a-clock-and-high-noon-concept-time-to-global-changes-elements-of-this-image.jpg?s=612x612&w=0&k=20&c=i4AIWe3ej5X4YyHNHKa-GJ8ld-GhoUb96lfhodCn4xE=",
+          "https://media.istockphoto.com/id/1473578427/photo/man-practicing-football-on-grass.jpg?s=612x612&w=0&k=20&c=7exLYAktSw_0teSSZ0EA05-nrMqeqBLIZnQ2Snt_lEM=",
+          "https://media.istockphoto.com/id/1134255601/photo/handsome-hispanic-man-wearing-casual-t-shirt-at-home-smiling-in-love-showing-heart-symbol-and.jpg?s=612x612&w=0&k=20&c=nFtss1CgiyzGKH9OE2aXztg-4BigMMzF-8sDpjw-6Wg=",
+          "https://media.istockphoto.com/id/540861476/photo/total-relaxation.jpg?s=612x612&w=0&k=20&c=LtGAz30Yt8xBfW3J8gAmCTaHL88yaV660nuK17SoXbc=",
+          "https://media.istockphoto.com/id/698788086/photo/indian-young-couple-celebrating-diwali.jpg?s=612x612&w=0&k=20&c=b0mj2nQfiqvm4uXIYjfciZ9FbAq6PrVKOPiiOVNFuUw=",
+        ]
+      : [
+          "https://media.istockphoto.com/id/1321486723/photo/japanese-man-spending-weekend-morning-painting-in-his-bedroom-at-home.jpg?s=612x612&w=0&k=20&c=vQNWXbNJ6xteAa33XmgBY4Uow0blGgCSl8gvRxuxSi4=",
+          "https://media.istockphoto.com/id/1342191952/photo/young-boy-preparing-for-exam-studying-at-home.jpg?s=612x612&w=0&k=20&c=WIDgjJgjP6xeziY6Pocoauu1a_pM3vQ8CSV_qr9FUYs=",
+          "https://media.istockphoto.com/id/1390285717/photo/asian-programmer-writing-code-on-a-laptop.jpg?s=612x612&w=0&k=20&c=mRD30S9Dlqws614cEzJ1Hieipf_O8HXv9Cppw1eXdjs=",
+          "https://media.istockphoto.com/id/172438443/photo/vintage-poetry-and-eyeglasses.jpg?s=612x612&w=0&k=20&c=FwbQPAL0MT33J5AGifmrsGG1V66KIT7j4SYrJWzTDRA=",
+          "https://media.istockphoto.com/id/1369836486/photo/adult-bearded-man-indoors-in-cafe-lifestyle-concept-photo-with-copy-space-picture-of-guy-who.jpg?s=612x612&w=0&k=20&c=KgtDm2-TI5tEM1xc0DD67RIUtsYBbC8AiriaGip1FX8=",
+        ];
+
+  images.forEach((image) => {
+    const imageContainer = document.createElement("div");
+    imageContainer.className = "life-interest-image-container";
+
+    const imgElement = document.createElement("img");
+    imgElement.src = image;
+    imgElement.alt = image;
+
+    imageContainer.appendChild(imgElement);
+    gallery.appendChild(imageContainer);
+  });
+
+  // Remove active class from all boxes
+  document.getElementById("life").classList.remove("active");
+  document.getElementById("interest").classList.remove("active");
+
+  // Add active class to the clicked box
+  document.getElementById(category).classList.add("active");
+}
+
+// Show 'Life' images by default when the page loads
+document.addEventListener("DOMContentLoaded", function () {
+  showImages("life");
+});
+
+//Code for search functionality
 document.addEventListener("DOMContentLoaded", function () {
   const searchForm = document.querySelector(".formNav");
   const searchInput = searchForm.querySelector("input");
@@ -97,65 +202,4 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     }
   });
-});
-
-function showImages(category) {
-  const gallery = document.getElementById("gallery");
-  gallery.innerHTML = "";
-
-  const images =
-    category === "life"
-      ? [
-          "https://media.istockphoto.com/id/1202312179/photo/hands-cup-holding-a-clock-and-high-noon-concept-time-to-global-changes-elements-of-this-image.jpg?s=612x612&w=0&k=20&c=i4AIWe3ej5X4YyHNHKa-GJ8ld-GhoUb96lfhodCn4xE=",
-          "https://media.istockphoto.com/id/1473578427/photo/man-practicing-football-on-grass.jpg?s=612x612&w=0&k=20&c=7exLYAktSw_0teSSZ0EA05-nrMqeqBLIZnQ2Snt_lEM=",
-          "https://media.istockphoto.com/id/1134255601/photo/handsome-hispanic-man-wearing-casual-t-shirt-at-home-smiling-in-love-showing-heart-symbol-and.jpg?s=612x612&w=0&k=20&c=nFtss1CgiyzGKH9OE2aXztg-4BigMMzF-8sDpjw-6Wg=",
-          "https://media.istockphoto.com/id/540861476/photo/total-relaxation.jpg?s=612x612&w=0&k=20&c=LtGAz30Yt8xBfW3J8gAmCTaHL88yaV660nuK17SoXbc=",
-          "https://media.istockphoto.com/id/698788086/photo/indian-young-couple-celebrating-diwali.jpg?s=612x612&w=0&k=20&c=b0mj2nQfiqvm4uXIYjfciZ9FbAq6PrVKOPiiOVNFuUw=",
-        ]
-      : [
-          "https://media.istockphoto.com/id/1321486723/photo/japanese-man-spending-weekend-morning-painting-in-his-bedroom-at-home.jpg?s=612x612&w=0&k=20&c=vQNWXbNJ6xteAa33XmgBY4Uow0blGgCSl8gvRxuxSi4=",
-          "https://media.istockphoto.com/id/1342191952/photo/young-boy-preparing-for-exam-studying-at-home.jpg?s=612x612&w=0&k=20&c=WIDgjJgjP6xeziY6Pocoauu1a_pM3vQ8CSV_qr9FUYs=",
-          "https://media.istockphoto.com/id/1390285717/photo/asian-programmer-writing-code-on-a-laptop.jpg?s=612x612&w=0&k=20&c=mRD30S9Dlqws614cEzJ1Hieipf_O8HXv9Cppw1eXdjs=",
-          "https://media.istockphoto.com/id/172438443/photo/vintage-poetry-and-eyeglasses.jpg?s=612x612&w=0&k=20&c=FwbQPAL0MT33J5AGifmrsGG1V66KIT7j4SYrJWzTDRA=",
-          "https://media.istockphoto.com/id/1369836486/photo/adult-bearded-man-indoors-in-cafe-lifestyle-concept-photo-with-copy-space-picture-of-guy-who.jpg?s=612x612&w=0&k=20&c=KgtDm2-TI5tEM1xc0DD67RIUtsYBbC8AiriaGip1FX8=",
-        ];
-
-  images.forEach((image) => {
-    const imageContainer = document.createElement("div");
-    imageContainer.className = "life-interest-image-container";
-
-    const imgElement = document.createElement("img");
-    imgElement.src = image;
-    imgElement.alt = image;
-
-    imageContainer.appendChild(imgElement);
-    gallery.appendChild(imageContainer);
-  });
-
-  // Remove active class from all boxes
-  document.getElementById("life").classList.remove("active");
-  document.getElementById("interest").classList.remove("active");
-
-  // Add active class to the clicked box
-  document.getElementById(category).classList.add("active");
-}
-
-// Show 'Life' images by default when the page loads
-document.addEventListener("DOMContentLoaded", function () {
-  showImages("life");
-});
-
-window.addEventListener("load", () => {
-  const loaderWrapper = document.getElementById("loader-wrapper");
-  const content = document.getElementById("content");
-
-  setTimeout(() => {
-    loaderWrapper.style.opacity = "0";
-    loaderWrapper.style.visibility = "hidden";
-    content.style.display = "block";
-    setTimeout(() => {
-      content.classList.add("loaded");
-      content.style.opacity = "1"; // Ensure content is fully visible
-    }, 100); // Slight delay to trigger the CSS transition
-  }, 3000); 
 });
